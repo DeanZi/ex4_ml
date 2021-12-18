fca596dc4f41fabb8b416e07d86ff50a453a41f3
deanzion <deanzion1@gmail.com>
2021-12-18 17:54:52 +0200

    added validation

--- ex_4.py
+++ ex_4.py
@@ -1,7 +1,7 @@
 import sys
 import numpy as np
 import torch
-from torch.utils.data import TensorDataset, DataLoader
+from torch.utils.data import TensorDataset, DataLoader, random_split
 import torch.nn.functional as F
 import torch.optim as optim
 from models_a_to_d import ModelType1
@@ -30,8 +30,9 @@ def receive_data(train_x, train_y, test_x):
     return train_x, train_y, test_x
 
 
-def train(model, train_loader, optimizer, epoch):
+def train(model, optimizer, epoch, validate, min_validation_loss=np.inf):
     for e in range(epoch):
+        train_loss = 0.0
         model.train()
         for batch_idx, (data, target) in enumerate(train_loader):
             optimizer.zero_grad()
@@ -39,45 +40,59 @@ def train(model, train_loader, optimizer, epoch):
             loss = F.nll_loss(output, target)
             loss.backward()
             optimizer.step()
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                e, batch_idx * len(data), len(train_loader.dataset),
-                   100. * batch_idx / len(train_loader), loss.item()))
+            train_loss += loss.item()
+        print('Train Epoch: {} \tLoss: {:.6f}'.format(
+            e + 1, train_loss / len(train_loader)))
+
+        if validate:
+            validation_loss = 0.0
+            model.eval()
+            for data, labels in validation_loader:
+                target = model(data)
+                loss = F.nll_loss(target, labels)
+                validation_loss += loss.item()
+
+            print(f'Validation Epoch {e + 1} \t\t Validation Loss: { validation_loss / len(validation_loader)}')
+
+            if min_validation_loss > validation_loss:
+                min_validation_loss = validation_loss
+                torch.save(model.state_dict(), 'saved_model.pth')
 
 
-def model_a(lr=0.01):
+def model_a(lr=0.01, validate=True):
     model = ModelType1(image_size=28 * 28)
     optimizer = optim.SGD(model.parameters(), lr=lr)
-    train(model, train_loader, optimizer, 10)
+    train(model, optimizer, 10, validate)
 
 
-def model_b(lr=0.01, use_dropout=False, use_batch_norm=False, batch_norm_before=False):
+def model_b(lr=0.01, use_dropout=False, use_batch_norm=False, batch_norm_before=False, validate=True):
     model = ModelType1(image_size=28 * 28, use_dropout=use_dropout, use_batch_norm=use_batch_norm,
                        batch_norm_before=batch_norm_before)
     optimizer = optim.Adam(model.parameters(), lr=lr)
-    train(model, train_loader, optimizer, 10)
+    train(model, optimizer, 10, validate)
 
 
-def model_c(lr=0.01):
-    model_b(lr, use_dropout=True)
+def model_c(lr=0.01, validate=True):
+    model_b(lr, use_dropout=True, validate=validate)
 
 
-def model_d(lr=0.01):
+def model_d(lr=0.01, validate=True):
     print("batch norm before")
-    model_b(lr, use_batch_norm=True, batch_norm_before=True)
+    model_b(lr, use_batch_norm=True, batch_norm_before=True, validate=validate)
     print("batch norm after")
-    model_b(lr, use_batch_norm=True, batch_norm_before=False)
+    model_b(lr, use_batch_norm=True, batch_norm_before=False, validate=validate)
 
 
-def model_e(lr=0.01):
+def model_e(lr=0.01, validate=True):
     model = ModelType2(image_size=28 * 28, model_letter='E')
     optimizer = optim.Adam(model.parameters(), lr=lr)
-    train(model, train_loader, optimizer, 10)
+    train(model, optimizer, 10, validate)
 
 
-def model_f(lr=0.01):
+def model_f(lr=0.01, validate=True):
     model = ModelType2(image_size=28 * 28, model_letter='F')
     optimizer = optim.Adam(model.parameters(), lr=lr)
-    train(model, train_loader, optimizer, 10)
+    train(model, optimizer, 10, validate)
 
 
 if __name__ == '__main__':
@@ -85,7 +100,9 @@ if __name__ == '__main__':
     train_x, test_x = normalize_data(train_x, test_x)
     train_x, train_y, test_x = data_to_tensors(train_x, train_y, test_x)
     data_set_train = TensorDataset(train_x, train_y)
-    train_loader = DataLoader(data_set_train, shuffle=True, batch_size=5)
+    train_set, validation_set = random_split(data_set_train, [8, 2])
+    train_loader = DataLoader(train_set, batch_size=2, shuffle=True)
+    validation_loader = DataLoader(validation_set)
     print('A')
     model_a()
     print('B')
